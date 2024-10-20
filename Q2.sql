6e6bf83e0cf65badffd3d43ad7c425ea012d7854
Vrund10 <200619743@student.georgianc.on.ca>
2024-10-20 13:52:39 -0400

    Add files via upload

---
+++ Q2.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE WeatherDB;
+USE WeatherDB;
+
+CREATE TABLE Weather (
+    id INT PRIMARY KEY,
+    recordDate DATE,
+    temperature INT
+);
+
+INSERT INTO Weather (id, recordDate, temperature) VALUES
+(1, '2015-01-01', 10),
+(2, '2015-01-02', 25),
+(3, '2015-01-03', 20),
+(4, '2015-01-04', 30);
